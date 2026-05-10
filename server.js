1a875bd338229eecace18142d7a1a3430d7982c1
Aman <amanalam769196@gmail.com>
2026-05-10 08:53:21 +0530

    fix port

--- server.js
+++ server.js
@@ -74,4 +74,4 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => console.log('Server running on port 3000'));
+http.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
