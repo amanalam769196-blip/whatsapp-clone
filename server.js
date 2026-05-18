6a981df11fe53ec81c6f118e6913f8f281c8df9f
Aman <amanalam769196@gmail.com>
2026-05-19 01:48:59 +0530

    Typing indicator add kiya

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("dotenv").config();
 require("dotenv").config();
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
